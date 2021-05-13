4d58cc96f06e607973d62daa68ffdb44d6b33ab9
Othman Sbai <otossbai@gmail.com>
2021-05-13 14:30:51 +0200

    update: gradientboosting classifier gives better perf > 0.8 on 1/10 of the data

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import argparse
+from time import time
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import GroupKFold,GroupShuffleSplit
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier,GradientBoostingClassifier
 from sklearn.metrics import roc_auc_score
 
 def parse_args():
@@ -11,17 +12,11 @@ def parse_args():
     parser.add_argument("-model", type=str, default="RandomForest", choices=['RandomForest', 'BoostedTree', 'LogistiRegression'], help="Model type to use")
     parser.add_argument("-nfolds", type=int, default=10, help="Number of cross validation folds")
     parser.add_argument("-seed", type=int, default=7, help="Seed for controlling randomness")
+    parser.add_argument("-nestimators", type=int, default=100, help="nestimators for random Forest and boosted Tree classifier")
+    parser.add_argument("-data_frac", type=float, default=1., help="fraction of data to use")
 
     args = parser.parse_args()
     return args
-
-def train(train_dset, args):
-    model = 1 # create model given args.model
-    return model
-
-def predict(model, val_dset):
-    score = 1
-    return score
 
 def fill_missing_values_binary_prob(col):
     """
@@ -53,7 +48,15 @@ def fill_missing_values(features):
 
     return features
 
-def load_training_data():
+def sample_customers(X, target, groups, frac=0.1):
+    kfold = GroupKFold(n_splits=int(1/frac))
+    train_index, val_index = list(kfold.split(X=X, groups=groups))[0]
+    sample_X = X.iloc[val_index]
+    sample_targets = target.iloc[val_index]
+    return sample_X, sample_targets, val_index
+
+
+def load_training_data(args):
     # load data
     df = pd.read_csv("data/labels_training.txt")
     views = pd.read_csv("data/views.txt")
@@ -73,9 +76,13 @@ def load_training_data():
     print("Dropping all rows with nan")
     df.dropna(inplace=True) # TODO: temporarily remove all rows with missing info
     print(df.shape)
-    # print("Sampling 0.1 fraction of rows")
-    # df = df.sample(frac=0.1)
-    # print(df.shape)
+    
+    frac = args.data_frac
+    if frac != 1.:
+        print(f"Sampling {frac} fraction of rows")
+        # df = df.sample(frac=frac) # sample randomly
+        df, _, sampled_inds = sample_customers(df, df['purchased'], df['customerId'], frac=frac) # sample by grouping customers
+        print(df.shape)
 
     target = df['purchased'].astype(int)
     customerId = df['customerId']
@@ -91,7 +98,7 @@ def main():
     np.random.seed(args.seed)
     
     print("Loading data ...")
-    features, target, customerId = load_training_data()
+    features, target, customerId = load_training_data(args)
 
     ## preprocessing features
     # fill missing values
@@ -118,6 +125,7 @@ def main():
 
     aucs = []
     for train_index, val_index in kfold.split(X=features, groups=customerId):
+        start = time()
         # print(train_index[:3], val_index[:3])
         # print(train_index.shape, val_index.shape)
 
@@ -131,7 +139,13 @@ def main():
         val_features = features.iloc[val_index]
         val_targets = target.iloc[val_index]
 
-        clf = RandomForestClassifier(random_state=args.seed)
+        if args.model == 'RandomForest':
+            clf = RandomForestClassifier(n_estimators=args.nestimators, random_state=args.seed)
+        elif args.model == 'BoostedTree':
+            clf = GradientBoostingClassifier(n_estimators=args.nestimators, random_state=args.seed)
+        else:
+            raise ValueError(f"Unkown model {args.model}")
+
         print("Fitting model ... ")
         clf.fit(train_features, train_targets)
         print("Predicting on val samples ... ")
@@ -140,15 +154,18 @@ def main():
         auc = roc_auc_score(val_targets, val_pred)
         print(auc)
         aucs.append(auc)
+        print(f"Took {(time()-start):0.3f}s")
         # for each row, append the customer info, product info, and the views info of customer and product
-    print(np.mean(aucs), np.std(aucs))
+    val_mean, val_std = np.mean(aucs), np.std(aucs)
+    
+    print(val_mean, val_std)
 
-
+    
     # use ensembling (bagging)
     # predict purchasing probabilities on the test data and save the prediction file
-    test_df = pd.read_csv("data/labels_predict.txt")
+    # test_df = pd.read_csv("data/labels_predict.txt")
 
-    return 1 # average cross validation score and filename of saved test results
+    return [np.mean(aucs), np.std(aucs)] # average cross validation score and filename of saved test results
     
 
 if __name__ == '__main__':
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# pip install pandas scikit-learn numpy
+
+python main.py -data_frac=0.1 -model=BoostedTree
